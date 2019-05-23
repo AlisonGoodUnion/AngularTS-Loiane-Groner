fa0129efbb04be95bba00587a76fdfccb212f3eb
Alison <alisonal1@hotmail.com>
2019-05-22 23:45:34 -0300

    Curso Angular #05: Módulos (ngModule)

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -1,20 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'; //BrowserModule prepara a aplicação pra ser executada em um browser
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';// a diretiva ngmodule faz parte desse pacote, é utilizada no two-way data binding
+//utilizado para valicaçoes de formularios
+import { HttpModule } from '@angular/http'; //modulo utilizado para requisições ajax.
+// para utilizar a dependencia do httpmodule se faz necessario o download dela atravez do comando npm i @angular/http
+// import { Observable } from 'rxjs';
+
+import { AppComponent } from './app.component';//imports das nossas classes
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
-
-import { AppComponent } from './app.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from 'src/app/cursos/cursos.module';
 
+//modulo raiz da aplicacao
 @NgModule({
-  declarations: [
-    AppComponent,
+  // para dizer que a class é um module utilizamos o decoratos NgModule
+  // necessario importar o angular core.
+  declarations: [// metadados declarados no module
+    AppComponent,//aqui sao declarados todos componentes diretivas e pipes que vamos utilizar no projeto
     MeuPrimeiroComponent,
     MeuPrimeiro2Component
   ],
-  imports: [
-    BrowserModule
+  imports: [ // no metadado de imports colocamos outros modulos que queremos utilizar nesse module
+    BrowserModule,//a ordem dos imports e versao tem impacto no funcionamento da aplicacao
+    HttpModule,
+    FormsModule,
+    // Observable
+    // é possivel criar seus proprios modules para não importar todas dependencias aqui
+    CursosModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], // providers recebem services que os componentes podem utilizar
+  //servicos de login, ou rotas geralmente ficam aqui, pois tem scope global...
+  bootstrap: [AppComponent]// aqui fica o view port o componente principal.
 })
 export class AppModule { }
+//em um modulo primeiro temos a class AppModule declaração conforme ecma2015
+// o Angular contem um style guide onde existe as convenções a serem seguidas
+//se verificarmos o nome da classe não é em camelcase igual o java
+//https://angular.io/guide/styleguide
---
+++ primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';// no module de funcionalidade se faz necessário importar o CommonModule
+// se o module necessida por exemplo realizar requests
+// http se faz necessário importar o httpmodule aqui também.
+
+import { CursosComponent } from './cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+
+@NgModule({//ao criar um novo component dentro do curso, automaticamento já é declarado 
+  //no seu ngmodule pai.
+  declarations: [CursosComponent, CursoDetalheComponent], // aqui declaramos nosso component no module se faz necessario importar
+  imports: [
+    CommonModule
+  ],
+  exports: [ // com essa diretive é possivel disponibilizar nosso component para toda a aplicação
+    CursosComponent
+  ]
+})
+export class CursosModule { }
+// ng g m cursos comando para gerar novo module
+// g: de generate m: de modulo
+// ng g c cursos comando para criar novo component que contem files css, html e ts
+// como o component tem o mesmo nome do module, os files criados pelo angular cli
+// acabam ficando no mesmo diretório, neste caso o CURSO
+//automaticamente é atualizado o app.module.ts
+// mas nesse caso vamos armazenar o nosso component no nosso module de cursos
