4451fdf66700feea056d8948277b852845374ff4
Alison <alisonal1@hotmail.com>
2019-07-17 23:42:00 -0300

    11: Event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -1,9 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  // styleUrls: ['./data-binding.component.css']
+  //podemos usar direto styles sem css file!
+  styles: [
+    `
+      .highlight {
+        background-color: brown
+      }
+    `
+  ]
 })
 export class DataBindingComponent implements OnInit {
 
@@ -11,8 +19,13 @@ export class DataBindingComponent implements OnInit {
   urlDaImagem: string = 'http://lorempixel.com/400/200/'
 
   cursosAngulas: boolean = true;
+  valorAtual: string;
+  valorSalvo: string;
 
-  constructor() { }
+  isMouseOuver: boolean = false;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
@@ -23,5 +36,23 @@ export class DataBindingComponent implements OnInit {
 
   getCurtirCurso(): boolean {
     return true;
+  }
+
+  botaoClicado() {
+    alert("botão click event binding!");
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    console.log((<HTMLInputElement>evento.target).value); //com o keyup tudo que é digitado é exibido no console log.
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvar(value: string) {
+    this.valorSalvo = value;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOuver = !this.isMouseOuver;
+    console.log(this.isMouseOuver);
   }
 }
