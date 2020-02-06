2e3a3f5768f94724e5368dbf29c2bb8b1d7393ca
Alison <alisonal1@hotmail.com>
2020-02-05 23:08:13 -0300

    20: Angular CLI: ng lint, ng test, ng e2e

--- diretivas/src/app/diretiva-ngif/diretiva-ngif.service.ts
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.service.ts
@@ -11,4 +11,9 @@ export class DiretivaNgifService {
 // pode ser criado projeto já com style default assim os novos arquivos de style dos component
 // vem no padrão.
 // ng set defaults.styleExt scss
+
+  // Angular CLI:
+  // ng lint = quality code veify
+  // ng test  = unit test
+  // ng e2e = end to end test
 }
